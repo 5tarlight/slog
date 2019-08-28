48d80378ceecff9218d3ea3a81a98fc165c62922
5tarlight <developerstarlight@gmail.com>
2019-08-29 07:46:31 +0900

    standard formatting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Portal } from './page'
+import { Header, Footer } from './component'
+import styled from 'styled-components'
 
-function App() {
+const Body = styled.div`
+  height: 100%;
+  width: 100%;
+`
+
+function App () {
   return (
     <BrowserRouter>
-      <Route exact path='/' component={Portal} />
+      <Header />
+
+      <Body>
+        <Route exact path='/' component={Portal} />
+      </Body>
+
+      <Footer />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/component/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 1.875rem;
+  background-color: white;
+`
+
+const Header = () => (
+  <HeaderWrapper />
+)
+
+export default Header
