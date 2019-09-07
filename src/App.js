f02f23ba4e9d5cb91ec84530d0f481e9d5e9b154
5tarlight <developerstarlight@gmail.com>
2019-09-07 17:19:51 +0900

    class component for using state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Portal, Login } from './page'
 import { Header } from './component'
 
-function App () {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Route exact path='/' component={Portal} />
-      <Route exact path='/login' component={Login} />
-    </BrowserRouter>
-  )
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Route exact path='/' component={Portal} />
+        <Route exact path='/login' component={Login} />
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App
