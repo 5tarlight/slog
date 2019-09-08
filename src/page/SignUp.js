81b6a96addd618aa0502abc2b933585df64af6b8
5tarlight <developerstarlight@gmail.com>
2019-09-08 10:54:53 +0900

    sign up page

---
+++ src/page/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ImageBackground } from '../component'
+
+const SignUp = () => (
+  <>
+    <ImageBackground />
+    <h1>ㅎㅇ</h1>
+  </>
+)
+
+export default SignUp
