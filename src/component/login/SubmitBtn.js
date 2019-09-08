cf8042699c13262a622c4f30056f89d85b6b2ea2
5tarlight <developerstarlight@gmail.com>
2019-09-08 11:30:07 +0900

    sign up input

--- src/component/login/SubmitBtn.js
+++ src/component/login/SubmitBtn.js
@@ -2,23 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Button = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
   font-size: 1rem;
   float: right;
   text-justify: center;
   text-align: center;
-  line-height: 6.5rem;
   background: rgb(100, 100, 100);
   color: white;
+  top: 50%;
+  transform: translateY(-50%);
 
   &:hover {
     cursor: pointer;
   }
+
+  p {
+    
+  }
 `
 
-const SubmitBtn = () => (
-  <Button>로그인</Button>
+const SubmitBtn = ({ children }) => (
+  <Button><p>{children || '로그인'}</p></Button>
 )
 
 export default SubmitBtn
--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -1,11 +1,39 @@
 import React from 'react'
-import { ImageBackground, MenuWrapper, Input } from '../component'
+import { ImageBackground, MenuWrapper, Input, SubmitBtn } from '../component'
+import styled from 'styled-components'
+
+const InputWrapper = styled.div`
+  width: 80%;
+  height: 100%;
+  float: left;
+`
+
+const ButtonWrapper = styled.div`
+  width: 19%;
+  height: 100%;
+  float: right;
+`
+
+const FormWrapper = styled.div`
+  width: 100%;
+  height: 156px;
+`
 
 const SignUp = () => (
   <>
     <ImageBackground />
     <MenuWrapper>
+      <FormWrapper>
+        <InputWrapper>
+          <Input placeholder='이메일' type='email'/>
+          <Input placeholder='비밀번호' type='password' />
+          <Input placeholder='비밀번호 확인' type='password' />
+        </InputWrapper>
 
+        <ButtonWrapper>
+          <SubmitBtn>회원가입</SubmitBtn>
+        </ButtonWrapper>
+      </FormWrapper>
     </MenuWrapper>
   </>
 )
