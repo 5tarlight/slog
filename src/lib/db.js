fb354121890518b8c50ccf01ab18add7d6a33a3e
5tarlight <developerstarlight@gmail.com>
2019-09-12 21:44:21 +0900

    database manager utility

---
+++ src/lib/db.js
@@ -0,0 +1,16 @@
+import { createConnection } from 'mysql'
+import * as config from './config.json'
+
+class DB {
+  static conn
+
+  static init() {
+    DB.conn = createConnection(config)
+  }
+
+  static query(query, callback) {
+    DB.conn.query(query, callback)
+  }
+}
+
+export default DB
