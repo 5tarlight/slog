548f171adb4ee65c0ceb95a77da3188b9dcb7113
5tarlight <developerstarlight@gmail.com>
2019-09-13 15:55:05 +0900

    insert feched data into database

--- server/api/auth/signUp.js
+++ server/api/auth/signUp.js
@@ -1,7 +1,22 @@
-
+const DB = require('../../lib/db')
+const qsb = require('node-qsb')
 
 module.exports.signup = ctx => {
   const { email, pw } = ctx.request.body
+
+  const params = {
+    cols: ['email', 'pw', 'verified'],
+    vals: [email, pw, 0]
+  }
+
+  const qs = new qsb()
+    .insert('user')
+    .values(params.cols, params.vals)
+    .build()
+  
+  DB.query(qs.returnString(), (err, results, fields) => {
+    if(err) throw err
+  }) 
 
   ctx.body = {
     value: 'hello'
